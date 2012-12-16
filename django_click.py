741f2fa49eb15343ca8fdaa73d240a6fa90d5aa9
starenka <starenka0@gmail.com>
2012-12-16 14:23:31 +0100

    - tag parsing supports multiple targets - added tests

--- django_click.py
+++ django_click.py
@@ -1,29 +1,26 @@
-import re, os
-
+import os
 import sublime, sublime_plugin
+from utils import parse_tag
+
 
 class DjangoClickCommand(sublime_plugin.TextCommand):
-	RE_BLOCK = re.compile(r'{%%\s+(?P<tag>%s)\s+[\'"]?(?P<name>[/\.\-_a-zA-Z0-9]+)[\'"]?\s+%%}' % 
-        	'|'.join(['include', 'extends', 'includeblocks']))
-	TEMPLATE_DIR = 'templates'
-	
+	TEMPLATE_DIR = 'templates'	
+
 	def run(self, edit):
 		region = self.view.sel()[0]
 		line = self.view.line(region)
 		line_contents = self.view.substr(line)
 
-		match = re.match(self.RE_BLOCK, line_contents)
-		
-		#is it the tag line we support?
-		if match:
-			tag, target =  match.groupdict()['tag'], match.groupdict()['name']
+		tag, targets = parse_tag(line_contents)
 
+		if tag:
 			# get the base-path of current file
 			base, current_file = self.view.file_name().split('%(separator)stemplates%(separator)s' % dict(separator=os.path.sep))
 
-			# get the target file path
-			tar = os.path.join(base, self.TEMPLATE_DIR, target)
+			for one in targets:
+				# get the target file path
+				tar = os.path.join(base, self.TEMPLATE_DIR, one) 
 
-			# open it!
-			window = sublime.active_window()
-			window.open_file(tar, sublime.ENCODED_POSITION)
+				# open it!
+				window = sublime.active_window()
+				window.open_file(tar, sublime.ENCODED_POSITION)
